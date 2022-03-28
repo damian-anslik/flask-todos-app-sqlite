28bac7e374acfbc74cb9e8c876d2b7dc5403cce5
damian-anslik <damiananslik@outlook.com>
2022-03-28 22:24:03 +0100

    init commit

---
+++ app/main.py
@@ -0,0 +1,66 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_required, current_user
+
+from app.models import Todo
+from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/todos')
+@login_required
+def todos():
+    """  
+    This function renders the todos page.
+    
+    args:
+        None
+    """
+    todos = Todo.query.filter_by(user_id=current_user.id).all()
+    return render_template('todos.html', todos=todos)
+
+@main.route('/todos', methods=['POST'])
+@login_required
+def add_todo():
+    """  
+    Add a todo to the database
+    args:
+        request: the request object
+    """
+    todo = Todo(description=request.form.get('todo_description'), user_id=current_user.id)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('main.todos'))
+
+@main.route('/todos/<int:todo_id>/delete', methods=['POST'])
+@login_required
+def delete_todo(todo_id: int):
+    """  
+    Delete a todo item
+    args:
+        todo_id: int
+    """
+    todo = Todo.query.get_or_404(todo_id)
+    if todo.user_id != current_user.id: # check if the user is the owner of the todo
+        return 'Forbidden', 403
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('main.todos'))
+
+@main.route('/todos/<int:todo_id>/set-completed', methods=['POST'])
+@login_required
+def set_completed_todo(todo_id: int):
+    """  
+    Set a todo item as completed
+    args:
+        todo_id: int
+    """
+    todo = Todo.query.get_or_404(todo_id)
+    if todo.user_id != current_user.id: # check if the user is the owner of the todo
+        return 'Forbidden', 403
+    todo.completed = True
+    db.session.commit()
+    return redirect(url_for('main.todos'))
---
+++ app/models.py
@@ -0,0 +1,32 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, index=True)
+    email = db.Column(db.String(128), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, name: str, email: str, password_hash: str):
+        self.name = name
+        self.email = email
+        self.password_hash = password_hash
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(128))
+    is_complete = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', backref=db.backref('todos', lazy='dynamic'))
+
+    def __init__(self, description: str, user_id: int):
+        self.description = description
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Todo %r>' % self.description
