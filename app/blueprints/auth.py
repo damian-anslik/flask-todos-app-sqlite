660a941e6744ac212d1392f6382ab7dff8c0568c
damian-anslik <damiananslik@outlook.com>
2022-04-03 11:45:07 +0100

    remove unnecessary email

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -1,4 +1,3 @@
-from uuid import uuid4
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from flask_mail import Message
