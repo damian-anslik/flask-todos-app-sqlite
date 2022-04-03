fbe3f3f4229bcb5ba2499706ce8ef0d5e854fccf
damian-anslik <damiananslik@outlook.com>
2022-04-03 01:37:36 +0100

    don't show user index page if logged in

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 def index():
+    # Don't show the index page if the user is logged in
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
     return render_template("index.html")
 
 @main.route("/home")
